2be67929c699fcfacccef9b21a88ad5f8e76d075
Ppm19 <pablo19ppm@gmail.com>
2025-02-10 18:25:25 +0100

    Mensajes Servicio

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,6 +6,7 @@ import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
 import { MessagesComponent } from '../messages/messages.component';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -23,7 +24,7 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
 
   ngOnInit() {
@@ -31,8 +32,9 @@ export class HeroesComponent implements OnInit {
   }
 
   selectedHero!: Hero;
-    onSelect(hero: Hero): void {
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
