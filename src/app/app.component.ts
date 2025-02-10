3633a04decd60a33f84ee0d49e3045db53e81c17
Ppm19 <pablo19ppm@gmail.com>
2025-02-10 16:51:43 +0100

    Lista de Heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent],
+  imports: [HeroesComponent, HeroDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'heroes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  hero : Hero = {
-   id: 1,
-   name: 'Windstorm'
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
   };
   constructor() { }
 
-  ngOnInit() {
 
+  ngOnInit() {
+    console.log('HeroesComponent inicializado.');
   }
 
+  selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
